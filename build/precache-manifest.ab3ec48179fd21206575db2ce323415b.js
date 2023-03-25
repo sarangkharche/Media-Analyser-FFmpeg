d5aac7517f82e57be48792b3a63b31100c8b0d69
Sarang Kharche <sarangkharche@gmail.com>
2023-03-25 16:08:42 +0000

    init

---
+++ build/precache-manifest.ab3ec48179fd21206575db2ce323415b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87b1f439170eda4ae29e",
+    "url": "/serverless-app/static/css/main.1b21bfd8.chunk.css"
+  },
+  {
+    "revision": "87b1f439170eda4ae29e",
+    "url": "/serverless-app/static/js/main.87b1f439.chunk.js"
+  },
+  {
+    "revision": "bc050cb1fdfaddf34e40",
+    "url": "/serverless-app/static/js/1.bc050cb1.chunk.js"
+  },
+  {
+    "revision": "01b8580073d06bd6a560",
+    "url": "/serverless-app/static/js/runtime~main.01b85800.js"
+  },
+  {
+    "revision": "7f314292e521ffb678a535ac64142917",
+    "url": "/serverless-app/index.html"
+  }
+];
